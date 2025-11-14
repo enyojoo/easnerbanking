11d6a7047dab2a2704028a5e6ab18bcf65e73a25
enyojoo <hello@enyosam.com>
2025-11-15 01:08:59 +0300

    Remove mobile header with logo from user dashboard layout

--- components/layout/user-dashboard-layout.tsx
+++ components/layout/user-dashboard-layout.tsx
@@ -113,13 +113,6 @@ export function UserDashboardLayout({ children }: UserDashboardLayoutProps) {
           </div>
         </div>
 
-        {/* Mobile Top bar */}
-        <div className="bg-white border-b border-gray-200 px-4 h-16 flex items-center sm:px-6 lg:hidden">
-          <div className="flex items-center justify-center w-full">
-            <BrandLogo size="sm" />
-          </div>
-        </div>
-
         {/* Page content */}
         <main className="flex-1 overflow-y-auto pb-20 lg:pb-0">{children}</main>
 
